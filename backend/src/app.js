4a1e647431795bf8d724abdccfe77d82a278c483
Vikas Kumar <vikumar162006@gmail.com>
2026-01-28 01:06:01 +0530

    Updated Frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,21 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import errorHandler from "./middleware/errorHandler.js";
 import prescriptionRoutes from "./routes/prescriptionRoutes.js";
 import medicineRoutes from "./routes/medicineRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-
 /* -------------------- MIDDLEWARES -------------------- */
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 /* -------------------- HEALTH CHECK -------------------- */
 app.get("/health", (req, res) => {
@@ -19,7 +24,9 @@ app.get("/health", (req, res) => {
     message: "Prescripto backend is running 🚀",
   });
 });
+
 /* -------------------- ROUTES -------------------- */
+app.use("/api/auth", authRoutes);
 app.use("/api/prescription", prescriptionRoutes);
 app.use("/api/medicine", medicineRoutes);
 
