4ac47e478fc1ab91ccec148f451aa060f8098bce
Vikas Kumar <vikumar162006@gmail.com>
2026-02-11 12:35:58 +0530

    Add live demo link to README

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lhldsm0sc58"
+    "revision": "0.qrj3sbhkss"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
