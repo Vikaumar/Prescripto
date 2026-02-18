3d3e26cc51d91d23deb82242f6a582f0fec487c5
Vikas Kumar <vikumar162006@gmail.com>
2026-02-18 23:36:04 +0530

    push

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qrj3sbhkss"
+    "revision": "0.vhj3kn93hbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
